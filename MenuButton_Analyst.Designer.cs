cd6de0da5b936cd7c31ec83ddca74108ebbe212a
KOBEJAMESMAWANAY <2024-2-00378@lpunetwork.edu.ph>
2026-05-02 22:52:15 +0800

    UPDATED MENU BUTTON

--- MenuButton_Analyst.Designer.cs
+++ MenuButton_Analyst.Designer.cs
@@ -30,55 +30,34 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuButton_Analyst));
             this.grpMenuButton_Analyst = new System.Windows.Forms.GroupBox();
-            this.dgvMenuButton_Analyst = new System.Windows.Forms.DataGridView();
             this.grbUserLogo_Analyst = new System.Windows.Forms.GroupBox();
             this.picUserPhoto_Analyst = new System.Windows.Forms.PictureBox();
             this.lblUserRole_Analyst = new System.Windows.Forms.Label();
             this.lblUserName_Analyst = new System.Windows.Forms.Label();
-            this.dgvMenuButtonLine2_Analyst = new System.Windows.Forms.Label();
             this.dgvMenuButtonLine1_Analyst = new System.Windows.Forms.Label();
             this.dgvMenuButtonLOGOUT_Analyst = new System.Windows.Forms.Button();
-            this.lblMenuButton_Analyst = new System.Windows.Forms.Label();
-            this.txtMenuButtonList_Analyst = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.txtMenuButtonSwitchUser_Analyst = new System.Windows.Forms.DataGridViewButtonColumn();
             this.grpMenuButton_Analyst.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuButton_Analyst)).BeginInit();
             this.grbUserLogo_Analyst.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUserPhoto_Analyst)).BeginInit();
             this.SuspendLayout();
             // 
             // grpMenuButton_Analyst
             // 
-            this.grpMenuButton_Analyst.Controls.Add(this.dgvMenuButton_Analyst);
             this.grpMenuButton_Analyst.Controls.Add(this.grbUserLogo_Analyst);
-            this.grpMenuButton_Analyst.Controls.Add(this.dgvMenuButtonLine2_Analyst);
             this.grpMenuButton_Analyst.Controls.Add(this.dgvMenuButtonLine1_Analyst);
             this.grpMenuButton_Analyst.Controls.Add(this.dgvMenuButtonLOGOUT_Analyst);
-            this.grpMenuButton_Analyst.Controls.Add(this.lblMenuButton_Analyst);
             this.grpMenuButton_Analyst.Location = new System.Drawing.Point(12, 12);
             this.grpMenuButton_Analyst.Name = "grpMenuButton_Analyst";
             this.grpMenuButton_Analyst.Size = new System.Drawing.Size(610, 587);
             this.grpMenuButton_Analyst.TabIndex = 2;
             this.grpMenuButton_Analyst.TabStop = false;
             // 
-            // dgvMenuButton_Analyst
-            // 
-            this.dgvMenuButton_Analyst.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvMenuButton_Analyst.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMenuButton_Analyst.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.txtMenuButtonList_Analyst,
-            this.txtMenuButtonSwitchUser_Analyst});
-            this.dgvMenuButton_Analyst.Location = new System.Drawing.Point(98, 173);
-            this.dgvMenuButton_Analyst.Name = "dgvMenuButton_Analyst";
-            this.dgvMenuButton_Analyst.Size = new System.Drawing.Size(412, 292);
-            this.dgvMenuButton_Analyst.TabIndex = 89;
-            // 
             // grbUserLogo_Analyst
             // 
             this.grbUserLogo_Analyst.Controls.Add(this.picUserPhoto_Analyst);
             this.grbUserLogo_Analyst.Controls.Add(this.lblUserRole_Analyst);
             this.grbUserLogo_Analyst.Controls.Add(this.lblUserName_Analyst);
-            this.grbUserLogo_Analyst.Location = new System.Drawing.Point(211, 42);
+            this.grbUserLogo_Analyst.Location = new System.Drawing.Point(211, 192);
             this.grbUserLogo_Analyst.Name = "grbUserLogo_Analyst";
             this.grbUserLogo_Analyst.Size = new System.Drawing.Size(183, 74);
             this.grbUserLogo_Analyst.TabIndex = 88;
@@ -115,19 +94,10 @@
             this.lblUserName_Analyst.TabIndex = 5;
             this.lblUserName_Analyst.Text = "Skibidi Toilet";
             // 
-            // dgvMenuButtonLine2_Analyst
-            // 
-            this.dgvMenuButtonLine2_Analyst.AutoSize = true;
-            this.dgvMenuButtonLine2_Analyst.Location = new System.Drawing.Point(95, 468);
-            this.dgvMenuButtonLine2_Analyst.Name = "dgvMenuButtonLine2_Analyst";
-            this.dgvMenuButtonLine2_Analyst.Size = new System.Drawing.Size(415, 13);
-            this.dgvMenuButtonLine2_Analyst.TabIndex = 10;
-            this.dgvMenuButtonLine2_Analyst.Text = "____________________________________________________________________";
-            // 
             // dgvMenuButtonLine1_Analyst
             // 
             this.dgvMenuButtonLine1_Analyst.AutoSize = true;
-            this.dgvMenuButtonLine1_Analyst.Location = new System.Drawing.Point(95, 119);
+            this.dgvMenuButtonLine1_Analyst.Location = new System.Drawing.Point(95, 269);
             this.dgvMenuButtonLine1_Analyst.Name = "dgvMenuButtonLine1_Analyst";
             this.dgvMenuButtonLine1_Analyst.Size = new System.Drawing.Size(415, 13);
             this.dgvMenuButtonLine1_Analyst.TabIndex = 9;
@@ -138,33 +108,12 @@
             this.dgvMenuButtonLOGOUT_Analyst.BackColor = System.Drawing.Color.Black;
             this.dgvMenuButtonLOGOUT_Analyst.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dgvMenuButtonLOGOUT_Analyst.ForeColor = System.Drawing.Color.White;
-            this.dgvMenuButtonLOGOUT_Analyst.Location = new System.Drawing.Point(98, 484);
+            this.dgvMenuButtonLOGOUT_Analyst.Location = new System.Drawing.Point(98, 298);
             this.dgvMenuButtonLOGOUT_Analyst.Name = "dgvMenuButtonLOGOUT_Analyst";
             this.dgvMenuButtonLOGOUT_Analyst.Size = new System.Drawing.Size(412, 46);
             this.dgvMenuButtonLOGOUT_Analyst.TabIndex = 8;
             this.dgvMenuButtonLOGOUT_Analyst.Text = "LOG OUT";
             this.dgvMenuButtonLOGOUT_Analyst.UseVisualStyleBackColor = false;
-            // 
-            // lblMenuButton_Analyst
-            // 
-            this.lblMenuButton_Analyst.AutoSize = true;
-            this.lblMenuButton_Analyst.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMenuButton_Analyst.Location = new System.Drawing.Point(114, 145);
-            this.lblMenuButton_Analyst.Name = "lblMenuButton_Analyst";
-            this.lblMenuButton_Analyst.Size = new System.Drawing.Size(364, 25);
-            this.lblMenuButton_Analyst.TabIndex = 3;
-            this.lblMenuButton_Analyst.Text = "ADMIN USER SWITCH CONTROL";
-            // 
-            // txtMenuButtonList_Analyst
-            // 
-            this.txtMenuButtonList_Analyst.HeaderText = "LIST";
-            this.txtMenuButtonList_Analyst.Name = "txtMenuButtonList_Analyst";
-            this.txtMenuButtonList_Analyst.ReadOnly = true;
-            // 
-            // txtMenuButtonSwitchUser_Analyst
-            // 
-            this.txtMenuButtonSwitchUser_Analyst.HeaderText = "SwitchUser";
-            this.txtMenuButtonSwitchUser_Analyst.Name = "txtMenuButtonSwitchUser_Analyst";
             // 
             // MenuButton_Analyst
             // 
@@ -176,7 +125,6 @@
             this.Text = "MenuButton_Analyst";
             this.grpMenuButton_Analyst.ResumeLayout(false);
             this.grpMenuButton_Analyst.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuButton_Analyst)).EndInit();
             this.grbUserLogo_Analyst.ResumeLayout(false);
             this.grbUserLogo_Analyst.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUserPhoto_Analyst)).EndInit();
@@ -187,16 +135,11 @@
         #endregion
 
         private System.Windows.Forms.GroupBox grpMenuButton_Analyst;
-        private System.Windows.Forms.DataGridView dgvMenuButton_Analyst;
-        private System.Windows.Forms.DataGridViewTextBoxColumn txtMenuButtonList_Analyst;
-        private System.Windows.Forms.DataGridViewButtonColumn txtMenuButtonSwitchUser_Analyst;
         private System.Windows.Forms.GroupBox grbUserLogo_Analyst;
         private System.Windows.Forms.PictureBox picUserPhoto_Analyst;
         private System.Windows.Forms.Label lblUserRole_Analyst;
         private System.Windows.Forms.Label lblUserName_Analyst;
-        private System.Windows.Forms.Label dgvMenuButtonLine2_Analyst;
         private System.Windows.Forms.Label dgvMenuButtonLine1_Analyst;
         private System.Windows.Forms.Button dgvMenuButtonLOGOUT_Analyst;
-        private System.Windows.Forms.Label lblMenuButton_Analyst;
     }
 }
